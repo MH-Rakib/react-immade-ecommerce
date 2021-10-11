cc04d8532bf7694bf72c9474f6f74b5e3b7626e7
Mehedi Hasan <mehedi.hasan4846@gmail.com>
2021-10-11 13:33:37 +0600

    Gitignored firebase config file

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -1,9 +1,46 @@
 import React from "react";
+import fakeData from "../../assets/fakeData";
 
 const Manage = () => {
+  const styles = {
+    width: "300px",
+    padding: "8px",
+    fontSize: "16px",
+    margin: "50px",
+  };
+
+  console.log(fakeData[0]);
+
+  const handleGet = () => {
+    fetch(`http://localhost:5000/`).then((res) => {
+      console.log(JSON.parse(res));
+    });
+  };
+
+  const handleUpload = () => {
+    fetch(`http://localhost:5000/addProduct`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fakeData),
+    });
+    // .then((res) => {
+    //   console.log(res);
+    // });
+  };
+
   return (
     <div>
-      <h2>Manage</h2>
+      <button onClick={handleUpload} style={styles}>
+        Upload products
+      </button>
+
+      <br />
+
+      <button onClick={handleGet} style={styles}>
+        Get products
+      </button>
     </div>
   );
 };
--- src/Components/Shipment/Shipment.js
+++ src/Components/Shipment/Shipment.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { UserContext } from "./../../App";
+import classes from "./Shipment.module.css";
 
 const Shipment = () => {
+  const [logedInUser, setLogedInUser] = useContext(UserContext);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
-    <div>
-      <h2>Shipment Page</h2>
-    </div>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+      <input
+        name="name"
+        {...register("name", { required: true })}
+        defaultValue={logedInUser.name}
+      />
+      {errors.name && (
+        <span className={classes.error}>This field is required</span>
+      )}
+      <br />
+
+      <input
+        name="email"
+        {...register("email", { required: true })}
+        defaultValue={logedInUser.email}
+      />
+      {errors.email && (
+        <span className={classes.error}>This field is required</span>
+      )}
+      <br />
+
+      <input
+        name="address"
+        {...register("address", { required: true })}
+        placeholder="Your address"
+      />
+      {errors.address && (
+        <span className={classes.error}>This field is required</span>
+      )}
+      <br />
+
+      <input
+        name="zip"
+        {...register("zip", { required: true })}
+        placeholder="Your Zip code"
+      />
+      {errors.zip && (
+        <span className={classes.error}>This field is required</span>
+      )}
+      <br />
+
+      <input type="submit" />
+    </form>
   );
 };
 
